612db1fc5a70d172cf130401e5bfbbe4f1cd959f
Adrian Fahrer <acfahrer@gmail.com>
2017-07-26 22:01:21 -0400

    upload

---
+++ keys.js
@@ -0,0 +1,9 @@
+
+
+exports.twitterKeys = {
+  consumer_key: '<4c6KjzGWrt1FyEr2VXVLzCm3Q>',
+  consumer_secret: 'eRZcZ9qYFMCw9hBeF7rZzZjoRZQAtqtbXUGFL3Ug0pzFnwcDYW>',
+  access_token_key: '<766597140-tx5HYIRx4UZNao6o7m0zOg1QCGDzURSpn1CiZDut>',
+  access_token_secret: '<reOi82WFdpw3sznQ6yBhPXOu4rdM6bTG9AyVqSjwahGn3>',
+}
+console.log('this is loaded');
